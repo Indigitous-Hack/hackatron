cd645add5b9d3ef3976e761bcfe7cb46d28df2f7
Bren Briggs <briggs.brenton@gmail.com>
2016-11-05 07:53:12 -0700

    Teach hackatron how to connect to slack over IRC

---
+++ hackatron.py
@@ -0,0 +1,38 @@
+import logging
+import threading
+import configparser
+from Legobot.Lego import Lego
+from Local.Roll import Roll
+
+from Legobot.Connectors.IRC import IRC
+from Legobot.Legos.Help import Help
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(ch)
+
+# Initialize lock and baseplate
+lock = threading.Lock()
+baseplate = Lego.start(None, lock)
+baseplate_proxy = baseplate.proxy()
+
+# Add children
+baseplate_proxy.add_child(IRC,
+                          channels=['#general','#hack'],
+                          nickname=config['kbmm']['username'],
+                          server=config['kbmm']['host'],
+                          port=int(config['kbmm']['port']),
+                          use_ssl=config.getboolean('kbmm','ssl'),
+                          username=config['kbmm']['username'],
+                          password=config['kbmm']['password'])
+baseplate_proxy.add_child(Help)
+baseplate_proxy.add_child(Roll)
